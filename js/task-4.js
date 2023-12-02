b525b78df1320a11f93534ea5dffae649a2322c7
jackavryashnik <jackavryashnik@gmail.com>
2023-12-02 20:36:31 +0200

    add initialValue for reduce

--- js/task-4.js
+++ js/task-4.js
@@ -1,43 +1,49 @@
-const getTotalBalanceByGender = (users, gender) => users.filter(user => user.gender === gender).map(e => e.balance).reduce((a, b) => a + b);
+const getTotalBalanceByGender = (users, gender) =>
+  users
+    .filter(user => user.gender === gender)
+    .map(e => e.balance)
+    .reduce((a, b) => {
+      return a + b;
+    }, 0);
 
 const users = [
-    {
-    name: "Moore Hensley",
-    gender: "male",
-    balance: 2811
+  {
+    name: 'Moore Hensley',
+    gender: 'male',
+    balance: 2811,
   },
   {
-    name: "Sharlene Bush",
-    gender: "female",
-    balance: 3821
+    name: 'Sharlene Bush',
+    gender: 'female',
+    balance: 3821,
   },
   {
-    name: "Ross Vazquez",
-    gender: "male",
-    balance: 3793
+    name: 'Ross Vazquez',
+    gender: 'male',
+    balance: 3793,
   },
   {
-    name: "Elma Head",
-    gender: "female",
-    balance: 2278
+    name: 'Elma Head',
+    gender: 'female',
+    balance: 2278,
   },
   {
-    name: "Carey Barr",
-    gender: "male",
-    balance: 3951
+    name: 'Carey Barr',
+    gender: 'male',
+    balance: 3951,
   },
   {
-    name: "Blackburn Dotson",
-    gender: "male",
-    balance: 1498
+    name: 'Blackburn Dotson',
+    gender: 'male',
+    balance: 1498,
   },
   {
-    name: "Sheree Anthony",
-    gender: "female",
-    balance: 2764
-  }
+    name: 'Sheree Anthony',
+    gender: 'female',
+    balance: 2764,
+  },
 ];
 
-console.log(getTotalBalanceByGender(users, "male")); // 12053
+console.log(getTotalBalanceByGender(users, 'male')); // 12053
 
-console.log(getTotalBalanceByGender(users, "female")); // 8863
+console.log(getTotalBalanceByGender(users, 'female')); // 8863
